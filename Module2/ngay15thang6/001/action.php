10646a2eeff9239353d36e3b4b470bb9ba32536a
dat <dathalongbay@gmail.com>
2020-06-15 18:42:52 +0700

    Q111

---
+++ Module2/ngay15thang6/001/action.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <h1>Đăng nhập thành công</h1>
+
+            <p>Chào admin bạn đã đăng nhập thành công</p>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ Module2/ngay15thang6/001/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 6/15/2020
+ * Time: 6:19 PM
+ */
