b4f11f2197b7f85a012f8934f67e3ac1cd95a276
dat <dathalongbay@gmail.com>
2020-06-29 19:41:42 +0700

    111

---
+++ Module2/ngay26thang6/002/baitap.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<pre>
+    Xây dựng 1 chương trình CRUD thêm sửa xóa sinh viên
+    gồm các dữ liệu sau
+    tên sinh viên ( kiểu varchar ) input text trong form
+    tuổi ( kiểu int ) từ 18 đến 30 tuổi ( sử dụng selectbox trong form )
+    giới tính ( kiểu tinyint ) chứa 2 giá trị 0 đại diện cho nữ
+    1 đại diện cho nam . kiểu radio trong form
+    quê quán ( varchar ) input text
+    điểm ( int ) input text
+    sử dụng cú pháp php mysql theo hướng hàm để làm 
+</pre>
+
+</body>
+</html>
