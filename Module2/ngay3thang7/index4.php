d4a46548ab12d70fae750b1b28f8124857358e50
dat <dathalongbay@gmail.com>
2020-07-03 19:35:22 +0700

    111

---
+++ Module2/ngay3thang7/index4.php
@@ -0,0 +1,31 @@
+<?php
+
+class HinhChuNhat {
+
+    public $chieudai;
+
+
+    public $chieurong;
+
+    // yêu cầu gán giá trị cho các thuộc tính trong constructor khởi tạo
+    public function __construct()
+    {
+
+
+    }
+
+
+    public function tinhDienTich() {
+
+    }
+
+
+    public function tinhChuVi() {
+
+    }
+
+}
+
+// khởi tạo đối tượng và in ra
+// dien tích và chu vi của 1 hcn có
+// chieu dai là 15 và chieu rong là 5
