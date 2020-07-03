decaac0d2c184d4135bc3c749144b0805d477402
dat <dathalongbay@gmail.com>
2020-07-03 18:43:45 +0700

    111

---
+++ Module2/ngay3thang7/index2.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class ImageHtml {
+
+    public $src;
+
+    public $alt;
+
+    public $width;
+
+    public $height;
+
+    public function outputHtml() {
+
+        // <img src='' alt='' width='' height='' />
+    }
+}
+
+// khởi tạo đối tượng
+
+// in ra hình ảnh
