bd57c7f3119d6447435684fc705e2ec7fb6f8e5c
dat <dathalongbay@gmail.com>
2020-07-03 19:12:17 +0700

    111

---
+++ Module2/ngay3thang7/index3.php
@@ -0,0 +1,38 @@
+<?php
+
+class InputText {
+
+    public $type;
+
+    public $name;
+
+    public $value;
+
+    public $placeholder;
+
+    public $style;
+
+    // khai báo phương thức khởi tạo của class
+    // phương thức khởi tạo khác với phương thức thông thường
+    // phương thức thông thường thì chủ động gọi nó mới chạy
+    // phương thức khởi tạo được tự động chạy ngay khi chúng ta
+    // khởi tạo đối tượng bằng từ khóa new
+    // chú ý có 2 shift trừ liên tiếp
+    public function __construct($type_param, $name_param, $value_param, $placeholder_param, $style_param)
+    {
+        echo '<br> kiểm chứng về tự động chạy';
+        $this->type = $type_param;
+        $this->name = $name_param;
+        $this->value = $value_param;
+        $this->placeholder = $placeholder_param;
+        $this->style = $style_param;
+    }
+
+}
+
+$input1 = new InputText('text', 'abc', '', 'Nhập demo input', '');
+
+// in ra cấu trúc của 1 object
+echo "<pre>";
+print_r($input1);
+echo "</pre>";
