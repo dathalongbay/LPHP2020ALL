c79d450254ee8db2fc287911352ef6b371148f7f
dat <dathalongbay@gmail.com>
2020-07-27 18:59:32 +0700

    111

---
+++ Module2/ngay27thang7/001/index1.php
@@ -0,0 +1,14 @@
+<?php
+
+$string = "Ha noi la thu do viet nam. Ha noi có khoang 8 triệu dân";
+// i là không phân biệt hoa thường
+// pattern bắt đầu / và kết thúc /
+$pattern = "/ha noi/i";
+$search = preg_match($pattern, $string);
+var_dump($search);
+
+
+// sử dụng preg_match_all
+$search_all = preg_match_all($pattern,$string);
+echo "<br>";
+var_dump($search_all);
